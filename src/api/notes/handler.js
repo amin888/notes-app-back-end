dc4307e7662766cbd5a9a5162f94d8f704ac0052
amin888 <amin888@gmail.com>
2023-01-19 09:12:29 +0700

    -

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -4,26 +4,99 @@ class NotesHandler {
 	}
 
 	postNoteHandler(request, h) {
-		const { title = "untitled", body, tags } = request.payload;
+		try {
+			const { title = "untitled", body, tags } = request.payload;
 
-		const noteId = this._service.addNote({ title, body, tags });
+			const noteId = this._service.addNote({ title, body, tags });
 
-		const response = h.response({
-			status: "success",
-			message: "Catatan berhasil ditambahkan",
-			data: {
-				noteId,
-			},
-		});
-		response.code(201);
-		return response;
+			const response = h.response({
+				status: "success",
+				message: "Catatan berhasil ditambahkan",
+				data: {
+					noteId,
+				},
+			});
+			response.code(201);
+			return response;
+		} catch (error) {
+			const response = h.response({
+				status: "fail",
+				message: error.message,
+			});
+			response.code(400);
+			return response;
+		}
 	}
 
-	getNotesHandler() {}
+	getNotesHandler() {
+		const notes = this._service.getNotes();
+		return {
+			status: "success",
+			data: {
+				notes,
+			},
+		};
+	}
 
-	getNoteByIdHandler() {}
+	getNoteByIdHandler(request, h) {
+		try {
+			const { id } = request.params;
+			const note = this._service.getNoteById(id);
+			return {
+				status: "success",
+				data: {
+					note,
+				},
+			};
+		} catch (error) {
+			const response = h.response({
+				status: "fail",
+				message: error.message,
+			});
+			response.code(404);
+			return response;
+		}
+	}
 
-	putNoteByIdHandler() {}
+	putNoteByIdHandler(request) {
+		try {
+			const { id } = request.params;
 
-	deleteNoteByIdHanlder() {}
+			this._service.editNoteById(id, request.payload);
+
+			return {
+				status: "success",
+				message: "Catatan berhasil diperbarui",
+			};
+		} catch (error) {
+			const response = h.response({
+				status: "fail",
+				message: error.message,
+			});
+			response.code(404);
+			return response;
+		}
+	}
+
+	deleteNoteByIdHanlder(request) {
+		try {
+			const { id } = request.params;
+
+			this._service.deleteNoteById(id);
+
+			return {
+				status: "success",
+				message: "Catatan berhasil dihapus",
+			};
+		} catch (error) {
+			const response = h.response({
+				status: "fail",
+				message: error.message,
+			});
+			response.code(404);
+			return response;
+		}
+	}
 }
+
+module.exports = NotesHandler;
