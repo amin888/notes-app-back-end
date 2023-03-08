1ab50122389d40286fda39d9bfecf2e046040ed2
Amin <amin888@gmail.com>
2023-03-08 09:21:49 +0700

    -

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,9 +1,27 @@
 const { Pool } = require("pg");
+const InvariantError = require("../../exceptions/InvariantError");
 
 class UsersService {
 	constructor() {
 		this._pool = new Pool();
 	}
 
-	async addUser({ username, password, fullname }) {}
+	async addUser({ username, password, fullname }) {
+		// TODO: verifikasi username, pastikan  belum terdaftar
+		await this.verifyNewUsername(username);
+		const id = `user-${nanoid(16)}`;
+		// TODO: bila verifikasi lolos, maka masukkan user baru ke database
+	}
+
+	async verifyNewUsername(username) {
+		const query = {
+			text: "SELECT username FROM users WHERE username = $1",
+			values: [username],
+		};
+		const result = await this._pool.query(query);
+
+		if (result.rows.length > 0) {
+			throw new InvariantError("Gagal menambahkan user. Username sudah diguanakan.");
+		}
+	}
 }
